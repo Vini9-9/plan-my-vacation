6a030c809c0183e3f6a853edf8c3bbdc64928a21
Vinicius Pessoa <vini.pessoa99@gmail.com>
2022-08-21 04:22:25 -0300

    Refatoração

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { GetFeriadosController } from './useCases/GetFeriados/GetFeriadosController';
 
+const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
     //console.log(moment.now().toLocaleString())
     console.log("Server is running")
 })
