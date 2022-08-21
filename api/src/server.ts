1721e1bc11d35f2e3025975a5859149f371c5012
Vinicius Pessoa <vini.pessoa99@gmail.com>
2022-08-21 03:44:47 -0300

    Evitar bind no heroku

--- api/src/server.ts
+++ api/src/server.ts
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     //console.log(moment.now().toLocaleString())
     console.log("Server is running")
 })
